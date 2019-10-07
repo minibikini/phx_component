a0827e5b12bc334809d94cb08a235a9ecda2acec
Egor Kislitsyn <egor@kislitsyn.com>
2019-10-08 01:00:20 +0700

    Add PhxComponent.table/2 tests

--- test/phx_component_test.exs
+++ test/phx_component_test.exs
@@ -1,5 +1,27 @@
 defmodule PhxComponentTest do
   use ExUnit.Case, async: true
 
+  import Phoenix.HTML
+
   doctest PhxComponent
+
+  @items [
+    %{id: 1, name: "foo"},
+    %{id: 2, name: "bar"}
+  ]
+
+  test "table/2" do
+    opts = [body: [:id, :name]]
+
+    html = PhxComponent.table(@items, opts) |> safe_to_string()
+
+    assert Floki.find(html, "table thead tr th") == [{"th", [], ["Id"]}, {"th", [], ["Name"]}]
+
+    assert Floki.find(html, "table tbody tr td") == [
+             {"td", [], ["1"]},
+             {"td", [], ["foo"]},
+             {"td", [], ["2"]},
+             {"td", [], ["bar"]}
+           ]
+  end
 end
