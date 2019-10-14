fbf633e49d7d59707cf82761fe038fe639c96b63
Egor Kislitsyn <egor@kislitsyn.com>
2019-10-14 21:26:47 +0700

    Add TableCardComponent

--- lib/phx_component.ex
+++ lib/phx_component.ex
@@ -6,4 +6,8 @@ defmodule PhxComponent do
   def table(collection, opts) do
     PhxComponent.TableComponent.render(collection, opts)
   end
+
+  def table_card(item, opts) do
+    PhxComponent.TableCardComponent.render(item, opts)
+  end
 end
